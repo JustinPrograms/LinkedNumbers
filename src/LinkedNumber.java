d6712ffce82d673d4fea171ebfdd574a49599549
JustinPrograms <83477376+JustinPrograms@users.noreply.github.com.>
2024-02-25 23:53:50 -0500

    Added Comments and Fixed Bugs

--- src/LinkedNumber.java
+++ src/LinkedNumber.java
@@ -1,126 +1,190 @@
 public class LinkedNumber {
 
-    private int base;
-    private DLNode<Digit> front;
-    private DLNode<Digit> rear;
+    private int base; // base of the number
+    private DLNode<Digit> front; // reference to the front node of the linked list
+    private DLNode<Digit> rear; // reference to the rear node of the linked list
 
+    /**
+     * Constructs a LinkedNumber object with the given number and base.
+     *
+     * @param num     the number to be represented
+     * @param baseNum the base of the number
+     */
     public LinkedNumber(String num, int baseNum) {
         this.base = baseNum;
         if (num.isEmpty()) throw new LinkedNumberException("no digits given");
-        Digit[] test;
 
-        this.front = new DLNode<>(new Digit(num.charAt(0)));
-        this.rear = this.front;
+        this.front = new DLNode<>(new Digit(num.charAt(0))); // create first node
+        this.rear = this.front; // initialize rear with front
 
         DLNode<Digit> current = this.front;
-        DLNode<Digit> nextNode = null;
+        DLNode<Digit> nextNode;
         int i = 1;
 
-        while (current != null) {
+        while (true) {
             try {
-                nextNode = new DLNode<>(new Digit(num.charAt(i)));
+                nextNode = new DLNode<>(new Digit(num.charAt(i))); // create next node
             } catch (StringIndexOutOfBoundsException e) {
-                this.rear = current;
-                this.rear.setNext(null); // added this
+                this.rear = current; // update rear to current node
+                this.rear.setNext(null); // set next of rear to null
                 break;
             }
 
-            current.setNext(nextNode);
-            nextNode.setPrev(current);
-            current = nextNode;
+            current.setNext(nextNode); // link current node to next node
+            nextNode.setPrev(current); // link next node to current node
+            current = nextNode; // move current to next node
             i++;
-
-
         }
-
     }
 
+    /**
+     * Constructs a LinkedNumber object with the given integer number (Base 10) by default.
+     *
+     * @param num the integer number to be represented
+     */
     public LinkedNumber(int num) {
-        this(String.valueOf(num), 10);
+        this(String.valueOf(num), 10); // call the constructor with the number converted to string and base 10
     }
 
+
+    /**
+     * Checks if the LinkedNumber represents a valid number.
+     *
+     * @return true if the number is valid, false otherwise
+     */
     public boolean isValidNumber() {
-
         DLNode<Digit> current = this.front;
+
+        // Iterate through each digit in the linked list
         while (current != null) {
-            if (current.getElement().getValue() < new Digit('0').getValue()){
-                return false;
+            // Check if the digit is less than 0
+            if (current.getElement().getValue() < new Digit('0').getValue()) {
+                return false; // if less than 0, return false
             }
-            if (current.getElement().getValue() > this.base-1) {
-                return false;
+            // Check if the digit is greater than or equal to the base
+            if (current.getElement().getValue() >= this.base) {
+                return false; // if greater than or equal to base, return false
             }
-            current = current.getNext();
+            current = current.getNext(); // move to the next digit
         }
-
-
-        return true;
+        return true; // return true if all digits are valid
     }
 
+
+    /**
+     * Retrieves the base of the LinkedNumber.
+     *
+     * @return the base of the LinkedNumber
+     */
     public int getBase() {
         return this.base;
     }
 
+    /**
+     * Retrieves the reference to the front node of the LinkedNumber.
+     *
+     * @return the reference to the front node of the LinkedNumber
+     */
     public DLNode<Digit> getFront() {
         return this.front;
     }
 
+    /**
+     * Retrieves the reference to the rear node of the LinkedNumber.
+     *
+     * @return the reference to the rear node of the LinkedNumber
+     */
     public DLNode<Digit> getRear() {
         return this.rear;
     }
 
+
+    /**
+     * Retrieves the number of digits in the LinkedNumber.
+     *
+     * @return the number of digits in the LinkedNumber
+     */
     public int getNumDigits() {
         int numOfNodes = 0;
         DLNode<Digit> current = this.front;
+
+        // Iterate through each node and count the number of digits
         while (current != null) {
             numOfNodes++;
             current = current.getNext();
         }
-        return numOfNodes;
+        return numOfNodes; // return the total number of digits
     }
 
+
+    /**
+     * Returns a string representation of the LinkedNumber.
+     *
+     * @return a string representation of the LinkedNumber
+     */
     public String toString() {
         StringBuilder output = new StringBuilder();
         DLNode<Digit> current = this.front;
+
+        // Iterate through each node and append the digit to the output
         while (current != null) {
             output.append(current.getElement());
             current = current.getNext();
         }
-        return output.toString();
+        return output.toString(); // return the string representation
     }
 
+
+    /**
+     * Checks if this LinkedNumber is equal to another LinkedNumber.
+     *
+     * @param other the other LinkedNumber to compare with
+     * @return true if the two LinkedNumbers are equal, false otherwise
+     */
     public boolean equals(LinkedNumber other) {
-
+        // Check if the bases are different
         if (this.base != other.base) return false;
+        // Check if the number of digits are different
         if (this.getNumDigits() != other.getNumDigits()) return false;
-        if (!this.toString().equals(other.toString())) return false;
-
-        return true;
+        // Check if the string representations are equal
+        return this.toString().equals(other.toString());
     }
 
+
+    /**
+     * Converts the LinkedNumber to a new base.
+     *
+     * @param newBase the base to convert the number to
+     * @return a new LinkedNumber representing the converted number
+     * @throws LinkedNumberException if the current number is invalid and cannot be converted
+     */
     public LinkedNumber convert(int newBase) {
         if (!this.isValidNumber()) {
             throw new LinkedNumberException("cannot convert invalid number");
         }
 
-        String newString = "";
+        String newString;
 
         if (this.base == 10) {
             newString = decimalToNewBase(newBase);
-         } else if (newBase == 10) {
+        } else if (newBase == 10) {
             newString = anyBaseToDecimal();
         } else {
             LinkedNumber temp = new LinkedNumber(anyBaseToDecimal(), 10);
             newString = temp.decimalToNewBase(newBase);
         }
 
-
         return new LinkedNumber(newString, newBase);
     }
 
+    /**
+     * Converts a decimal number to the specified base.
+     *
+     * @param newBase the base to convert to
+     * @return the converted number as a string
+     */
     private String decimalToNewBase(int newBase) {
-
-        char[] hexChars = "0123456789ABCDEF".toCharArray(); // Hexadecimal characters
-
+        char[] baseChars = "0123456789ABCDEF".toCharArray(); // Characters for different bases
         StringBuilder output = new StringBuilder();
         int decimalNum = Integer.parseInt(this.toString());
 
@@ -130,15 +194,19 @@ public class LinkedNumber {
 
         while (decimalNum > 0) {
             int remainder = decimalNum % newBase;
-            output.insert(0, hexChars[remainder]); // Insert remainder at the beginning
+            output.insert(0, baseChars[remainder]); // Insert remainder at the beginning
             decimalNum /= newBase;
         }
 
         return output.toString();
     }
 
+    /**
+     * Converts a number from any base to decimal.
+     *
+     * @return the decimal representation of the number as a string
+     */
     private String anyBaseToDecimal() {
-
         String currNum = this.toString();
         int total = 0;
 
@@ -146,10 +214,16 @@ public class LinkedNumber {
             total += Integer.parseInt(String.valueOf(currNum.charAt(i))) * powerOf(this.base, this.getNumDigits() - 1 - i);
         }
         return String.valueOf(total);
-
     }
 
-    private static int powerOf(int base, int exp) {
+    /**
+     * Computes the power of a number.
+     *
+     * @param base the base number
+     * @param exp  the exponent
+     * @return the result of base raised to the power of exp
+     */
+    private int powerOf(int base, int exp) {
         int res = 1;
         for (int i = 0; i < exp; i++) {
             res *= base;
@@ -158,26 +232,33 @@ public class LinkedNumber {
     }
 
 
+
+    /**
+     * Adds a digit at the specified position in the LinkedNumber.
+     *
+     * @param digit    the digit to add
+     * @param position the position at which to add the digit
+     * @throws LinkedNumberException if the position is invalid
+     */
     public void addDigit(Digit digit, int position) {
-        if (position > this.getNumDigits()|| position < 0) {
+        if (position > this.getNumDigits() || position < 0) {
             throw new LinkedNumberException("invalid position");
         }
 
         DLNode<Digit> current = this.rear;
         DLNode<Digit> newNode = new DLNode<>(digit);
 
-        if (position == this.getNumDigits()) {
+        if (position == this.getNumDigits()) { // Add at the end
             this.front.setPrev(newNode);
             newNode.setNext(this.front);
             this.front = newNode;
-        }
-
-        else if (position == 0) {
+            this.front.setPrev(null);
+        } else if (position == 0) { // Add at the beginning
             this.rear.setNext(newNode);
             newNode.setPrev(this.rear);
             this.rear = newNode;
-        }
-        else {
+            this.rear.setNext(null);
+        } else { // Add at the specified position
             int counter = 0;
             while (counter != position) {
                 current = current.getPrev();
@@ -188,43 +269,63 @@ public class LinkedNumber {
             newNode.setPrev(current);
             current.setNext(newNode);
         }
-
-
     }
 
+
+    /**
+     * Removes and returns the digit at the specified position in the LinkedNumber.
+     *
+     * @param position the position of the digit to remove
+     * @return the value of the removed digit multiplied by the appropriate power of the base
+     * @throws LinkedNumberException if the position is invalid
+     */
     public int removeDigit(int position) {
-        // everything works but need to figure out the return statement
-        if (position > this.getNumDigits()|| position < 0) {
+        if (position > this.getNumDigits() - 1 || position < 0) {
             throw new LinkedNumberException("invalid position");
         }
 
         DLNode<Digit> current = this.rear;
+        int value;
 
-        // remove last node
+        // Remove the last node
         if (position == 0) {
+            value = this.rear.getElement().getValue();
             DLNode<Digit> newRear = this.rear.getPrev();
-            this.rear.getPrev().setNext(null);
-            this.rear = newRear;
+            if (newRear != null) {
+                newRear.setNext(null);
+                this.rear = newRear;
+            } else {
+                this.front = null;
+                this.rear = null;
+            }
         }
-
+        // Remove the first node
         else if (position == this.getNumDigits() - 1) {
+            value = this.front.getElement().getValue();
             DLNode<Digit> newFront = this.front.getNext();
-            this.front.getNext().setPrev(null);
-            this.front.setNext(null);
-            this.front = newFront;
+            if (newFront != null) {
+                newFront.setPrev(null);
+                this.front.setNext(null);
+                this.front = newFront;
+            } else {
+                this.front = null;
+                this.rear = null;
+            }
         }
-
+        // Remove a node at a specific position
         else {
             int counter = 0;
             while (counter != position) {
                 current = current.getPrev();
                 counter++;
             }
-
+            value = current.getElement().getValue();
             current.getNext().setPrev(current.getPrev());
             current.getPrev().setNext(current.getNext());
         }
-        return 1;
+
+        return value * powerOf(this.base, position);
     }
+
 
 }
--- src/LinkedNumberException.java
+++ src/LinkedNumberException.java
@@ -1,7 +1,13 @@
-public class LinkedNumberException extends RuntimeException{
+/**
+ * LinkedNumberException is a custom RuntimeException used to indicate exceptional
+ * conditions related to linked numbers.
+ */
+public class LinkedNumberException extends RuntimeException {
 
+    /*
+     * @param msg the detail message (which is saved for later retrieval by the getMessage() method)
+     */
     public LinkedNumberException(String msg) {
         super(msg);
-
     }
 }
